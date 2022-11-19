8a6bf1db67981e877e43d2ff2b9bcec6eff64cf4
daniel.source <79308609+danielsource@users.noreply.github.com>
2022-11-19 11:38:19 -0300

    Add theme colors

--- config.h
+++ config.h
@@ -8,10 +8,10 @@ static const char *fonts[] = {
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	/*               fg         bg       */
+	[SchemeNorm] = { "#adadad", "#151515" },
+	[SchemeSel]  = { "#151515", "#597bc5" },
+	[SchemeOut]  = { "#000000", "#7dcfff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
